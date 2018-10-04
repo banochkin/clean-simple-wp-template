3f940435d035b7484a68655896deea48e2fe4e78
Ruslan Banochkin <ruslan@banochkin.com>
2018-10-05 01:38:29 +0300

    Убрал удаление REST API

--- template/functions.php
+++ template/functions.php
@@ -47,23 +47,4 @@ function showbreadcrumb() {
   if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="breadcrumbs">','</p>');}
 }
 
-// Disable REST API
-add_filter('rest_enabled', '__return_false');
-remove_action( 'xmlrpc_rsd_apis',            'rest_output_rsd' );
-remove_action( 'wp_head',                    'rest_output_link_wp_head', 10, 0 );
-remove_action( 'template_redirect',          'rest_output_link_header', 11, 0 );
-remove_action( 'auth_cookie_malformed',      'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_expired',        'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_username',   'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_hash',       'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_valid',          'rest_cookie_collect_status' );
-remove_filter( 'rest_authentication_errors', 'rest_cookie_check_errors', 100 );
-remove_action( 'init',          'rest_api_init' );
-remove_action( 'rest_api_init', 'rest_api_default_filters', 10, 1 );
-remove_action( 'parse_request', 'rest_api_loaded' );
-remove_action( 'rest_api_init',          'wp_oembed_register_route'              );
-remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4 );
-remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
-remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-
 ?>
