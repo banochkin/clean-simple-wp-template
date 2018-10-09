e9b198f73650609ffdeb79d29a317295840764ce
Ruslan Banochkin <ruslan@banochkin.com>
2018-10-09 23:16:08 +0300

    Update template/footer.php

--- template/footer.php
+++ template/footer.php
@@ -2,7 +2,7 @@
   <?php $args = array( 'theme_location' => 'footer', 'container'=> 'ul', 'menu_class' => 'footer-menu', 'menu_id' => 'footer-menu' );
     wp_nav_menu($args);
   ?>
-  <p><b><?php bloginfo('name'); ?></b>, <? echo date('Y'); ?></p>
+  <p><b><?php bloginfo('name'); ?></b>, <?php echo date('Y'); ?></p>
 </footer>
 <?php wp_footer(); ?>
 </body>
